bca911ebf975bdfcf4684ef66c1d59d6fca42cdb
varoteamulya <avarote@ncsu.edu>
2020-06-16 00:04:37 -0700

    Added Java backend project setup

---
+++ src/test/java/com/nfcOne/NfcOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfcOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfcOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
